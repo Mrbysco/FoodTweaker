aeb04d0cba32dcab220e5bc1964222aaf5f2e7ce
Mrbysco <Mrbysco@users.noreply.github.com>
2020-03-11 00:57:09 +0100

    -

--- src/main/java/com/mrbysco/foodtweaker/Reference.java
+++ src/main/java/com/mrbysco/foodtweaker/Reference.java
@@ -3,7 +3,7 @@ package com.mrbysco.foodtweaker;
 public class Reference {
 	public static final String MOD_ID = "foodtweaker";
 	public static final String MOD_NAME = "Food Tweaker";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "@VERSION@";
 	public static final String DEPENDENCIES = "required-after:crafttweaker;after:applecore;";
 
 	public static final String CLIENT_PROXY_CLASS = "com.mrbysco.foodtweaker.proxy.ClientProxy";
